b6012855d1754cca28e30e84dbac8595f6e04ef1
arersen <71315641+arersen@users.noreply.github.com>
2020-12-06 23:07:01 +0200

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,1 +1,4 @@
 import mathbyarersen as math #сокращения используем чисто для удобного использования
+type = input("Выберите которую вы функцию хотите использовать:\nlog\nsqrt\n")
+if type == "log":
+  input ==

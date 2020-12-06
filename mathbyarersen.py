529ddb99452cf37dcf7c2651640e391a003213a4
arersen <71315641+arersen@users.noreply.github.com>
2020-12-06 19:46:09 +0200

    Update mathbyarersen.py

--- mathbyarersen.py
+++ mathbyarersen.py
@@ -1,3 +1,9 @@
+#import mathbyarersen as math
+#print(math.sqrt(число,либо переменная)
 def sqrt(b):
 	a = b ** (1/2)
 	return a
+#///////////////////////////////////////////////
+def log(x,y):
+	#(x ** z) = y
+	

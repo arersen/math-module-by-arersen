30f36500e2fc2488a0ad7ea34c8abfa36d9762ee
arersen <71315641+arersen@users.noreply.github.com>
2020-12-06 23:13:46 +0200

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,4 +1,16 @@
 import mathbyarersen as math #сокращения используем чисто для удобного использования
-type = input("Выберите которую вы функцию хотите использовать:\nlog\nsqrt\n")
-if type == "log":
-  input ==
+while True:
+  type = input("Выберите которую вы функцию хотите использовать:\nlog\nsqrt\n")
+  if type == "log":
+    a = int(input("Введите по какому основанию будет логарифм "))
+    b = int(input("Введите число: "))
+    c = math.log(a,b)
+    print(c)
+  if type == "sqrt":
+    num = int(input("Введите число: "))
+    if num < 0:
+      print("ОШИБКА:Корня квадратного из отрицательного числа быть не может.")
+    elif num >= 0:
+      res = math.sqrt(num)
+      print(res)
+  
